be2412d5ff0b1ad015e96ca06e6c5a3706de50ae
Luke Robinett <lukebootcamp@gmail.com>
2020-09-04 22:15:16 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+ const chatSocket = new WebSocket("ws://localhost:3000");
+
+ chatSocket.onmessage = function (event) {
+  const message = JSON.parse(event.data);
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const timeStamp = `[${hours}:${minutes}]`;
+  const messageText = `${timeStamp} ${message.username}: ${message.message}`;
+  const newLi = $("<li tabindex='0' style='list-style-type: none;'>");
+
+  newLi.text(messageText);
+  $("#chat-output").append(newLi);
+ };
+
+ $("form").on("submit", function (event) {
+  event.preventDefault();
+
+  chatSocket.send(JSON.stringify({
+   username: $("#username").val(),
+   message: $("#chat-input").val()
+  }));
+  $("#chat-input").val("");
+ });
+
+ $("#username").on("keypress", function (event) {
+  const keyChar = String.fromCharCode(event.which);
+  const allowed = /[a-z0-9_]/i.test(keyChar);
+  if (!allowed) {
+   event.preventDefault();
+  }
+ });
+
+ $("#username").on("paste", function (event) {
+  event.preventDefault();
+ });
+});
